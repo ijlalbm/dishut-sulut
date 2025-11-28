05ff4a622ebdfb2cd22cd95eb4decfb013e706e4
ijlalbm <muhammadijlalm@gmail.com>
2025-11-28 16:39:30 +0800

    webgis

--- src/app/(full-width-pages)/webgis/page.tsx
+++ src/app/(full-width-pages)/webgis/page.tsx
@@ -2,17 +2,48 @@
 
 import dynamic from "next/dynamic";
 import Header from "../../../components/webgis/Header";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const LeafletMap = dynamic(() => import("../../../components/webgis/Map"), {
   ssr: false,
 });
 
+const images = [
+  "/images/carousel/carousel-01.png",
+  "/images/carousel/carousel-02.png",
+  "/images/carousel/carousel-03.png",
+];
+
 export default function HomePage() {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Header />
 
-      <div className="flex-1 my-1">
+      {/* Carousel full lebar atas */}
+      <div className="w-full">
+        <Carousel
+          showThumbs={false}
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          dynamicHeight={false}
+        >
+          {images.map((src, idx) => (
+            <div key={idx}>
+              <img
+                src={src}
+                alt={`Photo ${idx + 1}`}
+                className="object-cover w-full"
+                style={{ maxHeight: "70vh", width: "100%" }}
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+
+      {/* Map di bawah carousel, tinggi pasti dan responsif */}
+      <div className="w-full" style={{ height: "60vh" }}>
         <LeafletMap />
       </div>
     </div>
--- types/next.d.ts
+++ types/next.d.ts
@@ -1,6 +1,7 @@
 // types/next.d.ts (optional)
 import { NextApiRequest } from "next";
 
+declare module "*.css";
 declare module "next" {
   interface NextApiRequest {
     // you may add fields that middleware injects, e.g. userId
